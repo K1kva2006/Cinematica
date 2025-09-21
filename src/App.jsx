e21e32a398ae2f6917f412d677c8e15ae928d013
K1kva2006 <115255875+K1kva2006@users.noreply.github.com>
2025-09-21 13:42:12 +0400

    add error route

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ export default function App() {
           <Route path="/movie/:id/reservation" element={<Reservation/>}/>
           <Route path="/movie/:id/buyticket" element={<BuyTicket/>}/>
           <Route path="/movie/:id/payement" element={<Payement/>}/>
-          <Route path="*" element={<>meow</>} />
+          <Route path="*" element={<h1 style={{textAlign: "center", color: "red"}}>Error 404 Page Not Found</h1>} />
         </Routes>
       </Router>
     </>
